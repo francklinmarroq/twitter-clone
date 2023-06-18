81c1dbeb10e7fbed0865c92084a2a55c0a44dd21
Francklin Marroquin <francklin.marroquin@outlook.com>
2023-06-17 22:20:32 -0600

    Left sidebar basic

---
+++ components/sidebar/left/Tab.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <NuxtLink to="#" class="flex items-center py-3 px-6 w-min text-black hover:bg-gray-200 rounded-full dark:hover:bg-dim-200 dark:text-white" :class="defaultTransition">
+            <div class="w-8 h-8 text-dark">
+                <slot name="icon" />
+            </div>
+
+            <div class="ml-3 text-xl hidden xl:block" :class="textClasses">
+                <slot name="name" />
+            </div>
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup>
+const { defaultTransition } = useTailwindConfig();
+const props = defineProps({
+    active: {
+        type: Boolean,
+        default: false
+    }
+})
+const textClasses = computed(() => props.active ? 'font-semibold' : 'font-normal')
+
+</script>
---
+++ components/sidebar/left/index.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="h-screen flex flex-col">
+        <NuxtLink to="/">
+            <div class="p-2 my-2 hover:bg-blue-50 rounded-full w-min dark:hover:bg-white/20" :class="defaultTransition">
+                <div class="w-8 h-8">
+                    <LogoTwitter />
+                </div>
+            </div>
+        </NuxtLink>
+
+        <div class="mt-2 space-y-3">
+            <SidebarLeftTab active>
+                <template v-slot:icon>
+                    <IconHome />
+                </template>
+                <template v-slot:name>
+                    Home
+                </template>
+            </SidebarLeftTab>
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <IconSearch />
+                </template>
+                <template v-slot:name>
+                    Explore
+                </template>
+            </SidebarLeftTab>
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <IconMail />
+                </template>
+                <template v-slot:name>
+                    Messages
+                </template>
+            </SidebarLeftTab>
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <IconBell />
+                </template>
+                <template v-slot:name>
+                    Notifications
+                </template>
+            </SidebarLeftTab>
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <IconUser />
+                </template>
+                <template v-slot:name>
+                    Profile
+                </template>
+            </SidebarLeftTab>
+        </div>
+    </div>
+</template>
+<script setup>
+const { defaultTransition } = useTailwindConfig()
+</script>
+<style></style>
